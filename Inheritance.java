70b21dcaa1c1f281f9e960bfa177bc2cb6b49344
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2025-02-17 07:54:44 +0600

    inheritancs done

---
+++ Inheritance.java
@@ -0,0 +1,24 @@
+class Shape {
+    String color;
+}
+
+// The Triangle class inherits from the Shape class
+class triangle extends Shape {
+
+}
+// In Java, 4 types of inheritance are available:
+ // 1. Single Inheritance
+ // 2. Multilevel Inheritance
+ // 3. Hierarchical Inheritance
+ // 4. Hybrid Inheritance 
+
+public class Inheritance {
+
+    public static void main(String[] args) {
+        triangle t1 = new triangle();
+        t1.color = "red";
+
+
+    }
+
+}
